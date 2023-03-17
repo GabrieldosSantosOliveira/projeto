08e0e08405f2ef22e194337adf2e84f9483a609d
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-17 19:27:16 -0400

    feat: add component Text

---
+++ src/components/Text.tsx
@@ -0,0 +1,27 @@
+import { Slot } from '@radix-ui/react-slot';
+import { clsx } from 'clsx';
+import { ReactNode, FC } from 'react';
+interface TextProps {
+  size: 'sm' | 'md' | 'lg';
+  children: ReactNode;
+  asChild?: boolean;
+  className?: string;
+}
+export const Text: FC<TextProps> = ({ size, className, asChild, children }) => {
+  const Comp = asChild ? Slot : 'span';
+  return (
+    <Comp
+      className={clsx(
+        'font-Roboto font-medium text-gray-300',
+        {
+          'text-base': size === 'lg',
+          'text-sm': size === 'md',
+          'text-xs': size === 'sm',
+        },
+        className,
+      )}
+    >
+      {children}
+    </Comp>
+  );
+};
