25ed55e338bef6bb332ec020fd1e48093245137b
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-17 19:27:35 -0400

    feat: add _document

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,16 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600&family=Roboto:wght@400;500&display=swap"
+          rel="stylesheet"
+        />
       </Head>
       <body>
         <Main />
