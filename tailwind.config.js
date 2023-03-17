dceb9100ec6c047d3bb6fb4ec1046e542b7b10ee
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-17 19:26:49 -0400

    chore: add theme of application

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+const colors = require('tailwindcss/colors');
 module.exports = {
   content: ['./src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    colors: {
+      transparent: colors.transparent,
+      white: '#FFF',
+      amber: {
+        900: '#473404',
+      },
+      gray: {
+        900: '#191816',
+        700: '#24221F',
+        500: '#868686',
+        300: '#AFB6C2',
+        200: '#A9AFB9',
+      },
+      orange: {
+        900: '#FFC632',
+        500: '#FFDE88',
+        300: '#D4CCB6',
+      },
+    },
+    fontFamily: {
+      Poppins: 'Poppins',
+      Roboto: 'Roboto',
+    },
     extend: {},
   },
   plugins: [],
