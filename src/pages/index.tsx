b9dcf6dc9e603f09902e8e7cc348356a18887e22
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2023-03-17 12:27:47 -0400

    chore: add tailwind

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,11 @@
 import Head from 'next/head';
-import Image from 'next/image';
-
-import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div>
       <Head>
         <title>Camp In</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
-      <Image
-        className={styles.Imagem}
-        src="/images/profile.jpg"
-        alt="Imagem de um livro"
-        width={569}
-        height={1024}
-      />
     </div>
   );
 }
